fc27f5a2cec0f7aed6875190a61bebc7af93e4f6
rszubairi <r.s.zubairi@gmail.com>
2026-03-29 23:59:46 +0800

    google tags added

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
 import '../styles/globals.css';
 import { Providers } from './providers';
 
@@ -27,6 +28,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics Tag */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-DHXDWNDQHB"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-DHXDWNDQHB');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>
